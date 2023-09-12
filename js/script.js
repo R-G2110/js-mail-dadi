a20ad47956c8c856714925fa67a191f34bca49be
R-G2110 <argeldelacruz86@gmail.com>
2023-09-12 20:29:39 +0200

    [Add]: step 1

--- js/script.js
+++ js/script.js
@@ -1,17 +1,37 @@
 
 
-const registeredMail =  [rossi@gmail.it, verdi@gmail.it, marroni@libero.it];
-const mail = document.getElementById('').innerHTML;
-let mailValid = false;
+const registeredMail =  ['pippo@gmail.com', 'pappo@gmail.com', 'pino@gmail.com'];
+const inputName = document.getElementById('input-name');
+const inputMail = document.getElementById('input-mail');
+const control = document.getElementById('btn-control');
+const reset = document.getElementById('btn-reset');
+const outputMessage =  document.getElementById('output-message');
+let mailValid = false;                                    //condizione di default
+let message = 'Attenzione! questa mail non è registrata!'; //messaggio di default
 
-//1. Controllo se la mail inserita è nella lista
-for (let i=0; i < registeredMail.length; i++){
-	if (mail === registeredMail[i]){
-		//S'è nella lista visualizza il messasggio 'Benvenuto!'
-		mailValid = true;
-		console.log('Bentornato!');
-	} else {
-		//Se non è nella lista visualizza il messasggio 'Attenzione! questa mail non è registrata!'
-		console.log('Attenzione! questa mail non è registrata!');
+
+
+
+control.addEventListener('click', function(){
+	outputMessage.classList.remove('d-none');
+	
+	//1. Controllo se la mail inserita è nella lista
+	for (let i = 0; i < registeredMail.length && mailValid !== true; i++){
+		if (inputMail.value === registeredMail[i]){
+			//S'è nella lista visualizza il messasggio 'Benvenuto!'
+			mailValid = true;
+			message = 'Bentornato ' + inputName.value + '!';
+			console.log('Bentornato!');
+		}
 	}
-}
+
+	document.getElementById('output').innerHTML = message;
+})
+
+reset.addEventListener('click', function(){
+	inputName.value = '';
+	inputMail.value = '';
+	mailValid = false;
+	message = 'Attenzione! questa mail non è registrata!';
+	outputMessage.classList.add('d-none');
+})
